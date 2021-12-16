db97a277e20dae01f570f5c0421d7f72df60bd88
Stevej123 <jjanardhan@gmail.com>
2021-12-17 01:14:26 +0530

    first commit

--- js/validation.js
+++ js/validation.js
@@ -3,9 +3,9 @@ $(document).ready(function(){
     let passwordValid = false;
     let dateOfBirthValid = false;
 
-    let $username = $('input[name="username"]');
+    let $username = $('input[name="studentID"]');
     let $password = $('input[name="password"]');
-    let $dateOfBirth = $('input[name="dateOfBirth"]');
+    let $dateOfBirth = $('input[name="gradDate"]');
        $username.on('change', function(e){
         var pattern = /^[A-Z]/;      
         usernameValid =  pattern.test(this.value) 
@@ -30,7 +30,7 @@ $(document).ready(function(){
 
         if (usernameValid && passwordValid && dateOfBirthValid){
            
-            $('tbody').append(`<tr><td>${$('#username').val()}</td><td>${$('#password').val().length}</td><td>${$('#dateOfBirth').val()}</td></tr>`)
+            $('tbody').append(`<tr><td>${$('#studentID').val()}</td><td>${$('#password').val().length}</td><td>${$('#gradDate').val()}</td></tr>`)
             $('th').removeClass('ascending descending'); //new row added, so the table isn't sorted anymore
         }
     });
